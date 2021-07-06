7aa4e4dfbbad1f1592e043cc39062342e1b4e4bf
Aarush Goradia <80135238+Super45coder@users.noreply.github.com>
2021-07-06 16:52:55 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+menu_list_array = ["Veg Margherita Pizza", "Cheese Pizza", "Veg pizza", "Pepperoni pizza", "Hawaiian Pizza", "Meat pizza"];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0; i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>';
+}
+htmldata=htmldata + "</ol>";
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>";
+for(var i=0; i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card">' + '<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>';
+}
+htmldata=htmldata+"</section>";
+document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
+
+function add_top(){
+var item = document.getElementById("add_item").value;
+menu_list_array.push(item);
+add_item();
+}
